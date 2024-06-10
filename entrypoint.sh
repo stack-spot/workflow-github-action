543703cd6e6e572979378cf93c174b059ee4976a
Pedro Ribeiro <111748737+pedrohrfz@users.noreply.github.com>
2024-06-10 16:38:32 -0300

    Add extra inputs and refactor (#4)
    * add extra inputs and refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,20 +10,26 @@ idm_base_url=${7}
 workflow_api_base_url=${8}
 origin_branch=${9}
 feature_branch=${10}
+extra_inputs=${11}
 
 export client_id=$client_id
 export client_secret=$client_secret
 
-if [[ "$debug" == "true" ]]; then
-  echo "Debug Enabled"
-  export HTTP_ENABLE_FILE_DEBUG=true
-fi
-
-secret_stk_login=$(curl --location --request POST "$idm_base_url/realms/$realm/protocol/openid-connect/token" \
+secret_stk_login=$(curl -s --location --request POST "$idm_base_url/realms/$realm/protocol/openid-connect/token" \
     --header "Content-Type: application/x-www-form-urlencoded" \
     --data-urlencode "client_id=$client_id" \
     --data-urlencode "grant_type=client_credentials" \
     --data-urlencode "client_secret=$client_secret" | jq -r .access_token)
+
+put_workflow_url="$workflow_api_base_url/workflows/$execution_id"
+decoded_extra_inputs=$(echo $extra_inputs | base64 -d)
+put_workflow_json="{\"extra_inputs\": $decoded_extra_inputs}"
+http_code=$(curl --request PUT -s -o output.json -w '%{http_code}' "$put_workflow_url" --header "Authorization: Bearer $secret_stk_login" --header 'Content-Type: application/json' --data "$put_workflow_json";)
+if [[ "$http_code" -ne "200" ]]; then
+    echo "HTTP_CODE:" $http_code
+    echo "RESPONSE_CONTENT:" $(cat output.json)
+    exit $http_code
+fi
 
 url="$workflow_api_base_url/workflows/$execution_id?loginType=CLIENT_ACCESS"
 
@@ -37,18 +43,11 @@ fi
 
 http_code=$(curl -s -o script.sh -w '%{http_code}' "$url"  --header "Authorization: Bearer $secret_stk_login";)
 if [[ "$http_code" -ne "200" ]]; then
-    echo "------------------------------------------------------------------------------------------"
-    echo "---------------------------------------- Debug Starting ----------------------------------"
-    echo "------------------------------------------------------------------------------------------"
     echo "HTTP_CODE:" $http_code
     echo "RESPONSE_CONTENT:"
     cat script.sh
     exit $http_code
-    echo "------------------------------------------------------------------------------------------"
-    echo "---------------------------------------- Debug Ending ------------------------------------"
-    echo "------------------------------------------------------------------------------------------"
 else
-    echo "HTTP_CODE:" $http_code
     chmod +x script.sh
     echo "------------------------------------------------------------------------------------------"
     echo "---------------------------------------- Starting ----------------------------------------"
@@ -56,6 +55,9 @@ else
     echo "{\"outputs\": {\"created_repository\": \"$repo_url\"}}" > stk-local-context.json
     bash script.sh
     result=$?
+    if [[ "$debug" == "true" ]]; then
+        cat ~/.stk/debug/http.txt
+    fi
     echo "------------------------------------------------------------------------------------------"
     echo "----------------------------------------  Ending  ----------------------------------------"
     echo "------------------------------------------------------------------------------------------"
